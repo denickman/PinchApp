2c17acabe507e3ba1b9b55c4cea315fce655eedd
Dennis Yaremenko <yaremdennis@gmail.com>
2025-08-15 19:26:40 +0300

    Initial commit

---
+++ PinchApp/PinchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchAppApp.swift
+//  PinchApp
+//
+//  Created by Denis Yaremenko on 15.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct PinchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
